9241effdc49c14b7864981c2e331282b7da4f89b
melissaa55 <trenggonomeli@gmail.com>
2024-12-17 15:04:26 +0700

    feat: update profile & change navbar scooterb

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,30 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation"; // Import useRouter dan usePathname
 
 export default function Navbar() {
+  const router = useRouter();
+  const pathname = usePathname(); // Path halaman saat ini
   const [active, setActive] = useState("home");
+
+  // Periksa path saat pertama kali render
+  useEffect(() => {
+    if (pathname === "/home") setActive("home");
+    else if (pathname === "/maps") setActive("map");
+    else if (pathname === "/profile") setActive("profile");
+  }, [pathname]);
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-white border-gray-500 shadow-[0_-10px_10px_rgba(0,0,0,0.1)] flex justify-around items-center py-6">
       {/* Home */}
       <div
-        onClick={() => setActive("home")}
-        className={`flex flex-col items-center ${
+        onClick={() => {
+          setActive("home");
+          router.push("/home");
+        }}
+        className={`flex flex-col items-center cursor-pointer ${
           active === "home" ? "text-red-500" : "text-gray-400"
         }`}
       >
@@ -26,8 +39,11 @@ export default function Navbar() {
 
       {/* Map */}
       <div
-        onClick={() => setActive("map")}
-        className={`flex flex-col items-center ${
+        onClick={() => {
+          setActive("map");
+          router.push("/maps");
+        }}
+        className={`flex flex-col items-center cursor-pointer ${
           active === "map" ? "text-red-500" : "text-gray-400"
         }`}
       >
@@ -42,8 +58,11 @@ export default function Navbar() {
 
       {/* Profile */}
       <div
-        onClick={() => setActive("profile")}
-        className={`flex flex-col items-center ${
+        onClick={() => {
+          setActive("profile");
+          router.push("/profile");
+        }}
+        className={`flex flex-col items-center cursor-pointer ${
           active === "profile" ? "text-red-500" : "text-gray-400"
         }`}
       >
